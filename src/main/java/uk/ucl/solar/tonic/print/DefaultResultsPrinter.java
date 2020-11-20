21bf6889f0a928cb15de1ec60e3bc333d7756b1e
Giovani Guizzo <giovaniguizzo@gmail.com>
2020-11-20 16:56:05 +0000

    Adding the Printer

--- src/main/java/uk/ucl/solar/tonic/print/DefaultResultsPrinter.java
+++ src/main/java/uk/ucl/solar/tonic/print/DefaultResultsPrinter.java
@@ -17,43 +17,81 @@ package uk.ucl.solar.tonic.print;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-import gin.edit.Edit;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.Validate;
+import org.pmw.tinylog.Logger;
 import org.uma.jmetal.solution.Solution;
-import org.uma.jmetal.util.errorchecking.JMetalException;
+import uk.ucl.solar.tonic.exception.TonicException;
 
 /**
  *
  * @author Giovani
  */
-public class DefaultResultsPrinter {
+public class DefaultResultsPrinter<S extends Solution> implements Serializable {
 
     protected final File outputDir;
     protected String separator = ",";
     protected String varFileName = "VAR.csv";
     protected String funFileName = "FUN.csv";
     protected String timeFileName = "TIME.csv";
+    protected boolean shouldPrintHeaders = true;
 
-    protected List<? extends Solution<?>> solutionList;
+    protected List<S> solutionList;
+
+    protected List<String> objectiveNames;
     protected List<Boolean> isObjectiveToBeMinimized;
+
+    protected List<String> timeNames;
     protected List<Long> times;
 
-    public DefaultResultsPrinter(File outputDir, List<Solution<Edit>> solutionList, List<Long> times) {
+    public DefaultResultsPrinter(File outputDir) {
+        this.outputDir = outputDir;
+    }
+
+    public DefaultResultsPrinter(File outputDir, List<S> solutionList) {
+        this.outputDir = outputDir;
+        this.solutionList = solutionList;
+    }
+
+    public DefaultResultsPrinter(File outputDir, List<S> solutionList, List<Long> times) {
         this.outputDir = outputDir;
         this.solutionList = solutionList;
         this.times = times;
     }
 
-    public DefaultResultsPrinter(File outputDir, List<Solution<Edit>> solutionList, Long time) {
+    public DefaultResultsPrinter(File outputDir, List<S> solutionList, Long time) {
         this.outputDir = outputDir;
         this.solutionList = solutionList;
         this.times = Lists.newArrayList(time);
+    }
+
+    public DefaultResultsPrinter(File outputDir, List<S> solutionList, List<Long> times, boolean shouldWriteHeaders) {
+        this.outputDir = outputDir;
+        this.solutionList = solutionList;
+        this.times = times;
+        this.shouldPrintHeaders = shouldWriteHeaders;
+    }
+
+    public DefaultResultsPrinter(File outputDir, List<S> solutionList, Long time, boolean shouldWriteHeaders) {
+        this.outputDir = outputDir;
+        this.solutionList = solutionList;
+        this.times = Lists.newArrayList(time);
+        this.shouldPrintHeaders = shouldWriteHeaders;
+    }
+
+    public boolean getShouldWriteHeaders() {
+        return shouldPrintHeaders;
+    }
+
+    public void setShouldPrintHeaders(boolean shouldPrintHeaders) {
+        this.shouldPrintHeaders = shouldPrintHeaders;
     }
 
     public String getVarFileName() {
@@ -80,11 +118,11 @@ public class DefaultResultsPrinter {
         this.timeFileName = timeFileName;
     }
 
-    public List<? extends Solution<?>> getSolutionList() {
+    public List<S> getSolutionList() {
         return solutionList;
     }
 
-    public void setSolutionList(List<? extends Solution<?>> solutionList) {
+    public void setSolutionList(List<S> solutionList) {
         this.solutionList = solutionList;
     }
 
@@ -116,86 +154,138 @@ public class DefaultResultsPrinter {
         this.separator = separator;
     }
 
-    public void print() {
-        printObjectivesToFile();
-        printVariablesToFile();
-        printTimesTofile();
+    public List<String> getObjectiveNames() {
+        return objectiveNames;
     }
 
-    public void printVariablesToFile() {
-        FileWriter writer = null;
+    public void setObjectiveNames(List<String> objectiveNames) {
+        this.objectiveNames = objectiveNames;
+    }
+
+    public List<String> getTimeNames() {
+        return timeNames;
+    }
+
+    public void setTimeNames(List<String> timeNames) {
+        this.timeNames = timeNames;
+    }
+
+    public void print() throws IOException, TonicException {
+        this.createOutputDir();
+        this.printObjectivesToFile();
+        this.printVariablesToFile();
+        this.printTimesTofile();
+    }
+
+    protected void createOutputDir() throws IOException {
+        Validate.notNull(outputDir);
         try {
-            writer = new FileWriter(FileUtils.getFile(outputDir, varFileName));
-            for (Solution<?> solution : solutionList) {
-                StringBuilder lineBuilder = new StringBuilder();
-                Joiner.on(separator).appendTo(lineBuilder, solution.getVariables());
-                lineBuilder.append(System.lineSeparator());
-                writer.write(lineBuilder.toString());
-            }
-        } catch (IOException e) {
-            throw new JMetalException("Error writing data ", e);
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(DefaultResultsPrinter.class.getName()).log(Level.SEVERE, null, ex);
+            Files.createDirectories(outputDir.toPath(), null);
+        } catch (IOException ex) {
+            Logger.error(ex, "Error creating output directory for the results: " + outputDir.getAbsolutePath());
+            throw ex;
+        }
+    }
+
+    protected void printLine(List<?> elements, FileWriter writer) throws IOException {
+        StringBuilder lineBuilder = new StringBuilder();
+        Joiner.on(separator).appendTo(lineBuilder, elements);
+        this.printLine(lineBuilder.toString(), writer);
+    }
+
+    protected void printLine(String line, FileWriter writer) throws IOException {
+        try {
+            writer.write(line);
+            writer.write(System.lineSeparator());
+        } catch (IOException ex) {
+            Logger.error(ex, "Error writing data to file.");
+            throw ex;
+        }
+    }
+
+    protected void printHeader(List<String> columns, FileWriter writer) throws IOException {
+        if (shouldPrintHeaders) {
+            this.printLine(columns, writer);
+        }
+    }
+
+    protected void printVariablesToFile() throws IOException {
+        if (solutionList != null && !solutionList.isEmpty()) {
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(FileUtils.getFile(outputDir, varFileName));
+                for (Solution<?> solution : solutionList) {
+                    printLine(solution.getVariables(), writer);
+                }
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException ex) {
+                        Logger.error(ex, "Error closing file stream.");
+                    }
                 }
             }
         }
     }
 
-    public void printObjectivesToFile() {
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(FileUtils.getFile(outputDir, funFileName));
-            if (!solutionList.isEmpty()) {
+    protected void printObjectivesToFile() throws IOException, TonicException {
+        if (solutionList != null && !solutionList.isEmpty()) {
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(FileUtils.getFile(outputDir, funFileName));
                 int numberOfObjectives = solutionList.get(0).getNumberOfObjectives();
-                if (isObjectiveToBeMinimized != null && numberOfObjectives != isObjectiveToBeMinimized.size()) {
-                    throw new JMetalException("The size of list minimizeObjective is not correct: " + isObjectiveToBeMinimized.size());
+                if (isObjectiveToBeMinimized != null && isObjectiveToBeMinimized.size() != numberOfObjectives) {
+                    throw new TonicException("The size of list minimizeObjective is not correct: " + isObjectiveToBeMinimized.size());
+                }
+                if (objectiveNames != null && !objectiveNames.isEmpty()) {
+                    if (objectiveNames.size() != numberOfObjectives) {
+                        throw new TonicException("The size of list objectiveNames is not correct: " + objectiveNames.size());
+                    }
+                    this.printHeader(objectiveNames, writer);
                 }
                 for (Solution<?> solution : solutionList) {
-                    String line = "";
-                    Joiner joiner = Joiner.on(separator);
+                    List<String> objectives = new ArrayList<>(numberOfObjectives);
                     for (int j = 0; j < numberOfObjectives; j++) {
                         if (isObjectiveToBeMinimized == null || isObjectiveToBeMinimized.get(j)) {
-                            line = joiner.join(line, solution.getObjective(j));
+                            objectives.add(String.valueOf(solution.getObjective(j)));
                         } else {
-                            line = joiner.join(line, -1.0 * solution.getObjective(j));
+                            objectives.add(String.valueOf(-1.0 * solution.getObjective(j)));
                         }
                     }
-                    writer.write(line + System.lineSeparator());
+                    this.printLine(objectives, writer);
                 }
-            }
-        } catch (IOException e) {
-            throw new JMetalException("Error writing data ", e);
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(DefaultResultsPrinter.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException ex) {
+                        Logger.error(ex, "Error closing file stream.");
+                    }
                 }
             }
         }
     }
 
-    public void printTimesTofile() {
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(FileUtils.getFile(outputDir, timeFileName));
-            StringBuilder lineBuilder = new StringBuilder();
-            Joiner.on(separator).appendTo(lineBuilder, times);
-            lineBuilder.append(System.lineSeparator());
-            writer.write(lineBuilder.toString());
-        } catch (IOException e) {
-            throw new JMetalException("Error writing data ", e);
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(DefaultResultsPrinter.class.getName()).log(Level.SEVERE, null, ex);
+    protected void printTimesTofile() throws IOException, TonicException {
+        if (times != null && !times.isEmpty()) {
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(FileUtils.getFile(outputDir, timeFileName));
+                if (timeNames != null && !timeNames.isEmpty()) {
+                    if (timeNames.size() != times.size()) {
+                        throw new TonicException("The size of list timeNames is not correct: " + timeNames.size());
+                    }
+                    this.printHeader(timeNames, writer);
+                }
+                printLine(times, writer);
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException ex) {
+                        Logger.error(ex, "Error closing file stream.");
+                    }
                 }
             }
         }
---
+++ src/main/java/uk/ucl/solar/tonic/print/GIResultsPrinter.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2020 Giovani.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ucl.solar.tonic.print;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.io.FileUtils;
+import org.pmw.tinylog.Logger;
+import uk.ucl.solar.tonic.exception.TonicException;
+import uk.ucl.solar.tonic.solution.PatchSolution;
+
+/**
+ *
+ * @author Giovani
+ */
+public class GIResultsPrinter extends DefaultResultsPrinter<PatchSolution> {
+
+    protected List<String> patchColumnsNames;
+    protected String patchFileName = "PATCH.csv";
+
+    public GIResultsPrinter(List<String> columns, File outputDir, List<PatchSolution> solutionList, List<Long> times) {
+        super(outputDir, solutionList, times);
+        this.patchColumnsNames = columns;
+    }
+
+    public GIResultsPrinter(List<String> columns, File outputDir, List<PatchSolution> solutionList, Long time) {
+        super(outputDir, solutionList, time);
+        this.patchColumnsNames = columns;
+    }
+
+    public List<String> getPatchColumnsNames() {
+        return patchColumnsNames;
+    }
+
+    public void setPatchColumnsNames(List<String> patchColumnsNames) {
+        this.patchColumnsNames = patchColumnsNames;
+    }
+
+    public String getPatchFileName() {
+        return patchFileName;
+    }
+
+    public void setPatchFileName(String patchFileName) {
+        this.patchFileName = patchFileName;
+    }
+
+    @Override
+    public void print() throws IOException, TonicException {
+        super.print();
+        printPatchesToFile();
+    }
+
+    private void printPatchesToFile() throws TonicException, IOException {
+        if (solutionList != null && !solutionList.isEmpty()) {
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(FileUtils.getFile(outputDir, patchFileName));
+                if (patchColumnsNames != null && !patchColumnsNames.isEmpty()) {
+                    this.printHeader(patchColumnsNames, writer);
+                }
+                for (PatchSolution solution : solutionList) {
+                    List<?> attributes = patchColumnsNames.stream()
+                            .map(column -> solution.getAttribute(column))
+                            .collect(Collectors.toList());
+                    this.printLine(attributes, writer);
+                }
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException ex) {
+                        Logger.error(ex, "Error closing file stream.");
+                    }
+                }
+            }
+        }
+    }
+
+}
